e5cb50a1daca728d2c029401ec1162191e5d3191
Kalyani_karri <venkatakalyanikarri913@gmail.com>
2026-03-24 10:06:36 +0530

    Add files via upload

---
+++ ThrowsDemo.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class ThrowsDemo{
+  static void readFile() throws IOException{
+FileReader fr = new FileReader("file.txt");
+
+}
+public static void main(String args[])
+{
+try {
+readFile();
+
+}
+catch(IOException e)
+{
+ System.out.println("File not found");
+}
+}
+}
