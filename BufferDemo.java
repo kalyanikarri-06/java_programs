690a496bb6f0cf3629027ea85d8bc378c3f6ec37
Kalyani_karri <venkatakalyanikarri913@gmail.com>
2026-03-24 10:05:28 +0530

    Add files via upload

---
+++ BufferDemo.java
@@ -0,0 +1,23 @@
+import java.io.*;
+class BufferDemo
+{
+public static void main(String args[])throws IOException
+{
+//write
+BufferedWriter bw= new BufferedWriter(new FileWriter(" sample.txt"));
+bw.write("hello sample file");
+bw.newLine();
+bw.write("Buffered writer Class");
+bw.close();
+System.out.println("Text is written in the sample.txt");
+//Read
+BufferedReader br= new BufferedReader(new FileReader("sample.txt"));
+String line;
+System.out.println("Read the data from txt file");
+while((line=br.readLine())!=null)
+{
+System.out.println(line);
+}
+br.close();
+}
+}
